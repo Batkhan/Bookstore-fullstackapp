bf780e4e9e11ecb0ed40d8b9687f82dec3b383aa
Batkhan <hamdanikutty@gmail.com>
2025-08-10 11:19:04 +0530

    Delete all files from main branch

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import {Routes, Route} from 'react-router-dom';
-
-<Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/add" element={<AddBook />} />
-    <Route path="/edit/:id" element={<EditBook />} />
-</Routes>
